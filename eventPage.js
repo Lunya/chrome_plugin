ab7955e7a15287d523069fd7ff7d25dd8e65391b
Xavier Blanc <xavier.blanc@labri.fr>
2017-09-01 20:11:32 +0200

    goto and click are ok

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,58 @@
-scenario = [];
-isRecording = false;
+class PageManager {
+	constructor() {
+		this.scenario = [];
+		this.isRecording = false;
+	}
 
-chrome.runtime.onMessage.addListener(addActionToScenario);
+	start() {
+		chrome.runtime.onMessage.addListener(this.addActionToScenario.bind(this));
+	}
 
-function addActionToScenario(action) {
-	scenario.push(action);
+	startRecording() {
+		this.isRecording = true;
+		chrome.webNavigation.onCommitted.addListener(webNavigationCommitted);
+		chrome.webNavigation.onCompleted.addListener(webNavigationCompleted);
+	
+		chrome.tabs.query({active: true, currentWindow: true}, activeTabs => {
+			if (activeTabs.length && activeTabs.length > 0) {
+				chrome.tabs.reload(activeTabs[0].id);
+			}
+		});
+	}
+
+	stopRecording() {
+		this.isRecording = false;
+		this.scenario = [];
+		chrome.webNavigation.onCommitted.removeListener(webNavigationCommitted);
+		chrome.webNavigation.onCompleted.removeListener(webNavigationCompleted);
+	}
+
+	addActionToScenario(action) {
+		alert('receive msg:'+action);
+		this.scenario.push(action);
+	}
 }
+
+function webNavigationCommitted({transitionType, url}) {
+	chrome.tabs.query({active: true, currentWindow: true}, activeTabs => {
+		if (transitionType === 'reload' || transitionType === 'start_page') {
+			if (activeTabs.length && activeTabs.length > 0) {
+				//console.log('send msg goto');
+				//chrome.runtime.sendMessage('Goto' + url);
+				pageManager.scenario.push('Goto '+url);
+				alert('send msg goto');
+			}
+		}
+	});  
+}
+
+function webNavigationCompleted() {
+	chrome.tabs.query({active: true}, activeTabs => {
+		if (activeTabs.length && activeTabs.length > 0) {
+			chrome.tabs.executeScript(activeTabs[0].id , {file:'attachListener.js'});
+		}
+	});
+}
+
+var pageManager = new PageManager();
+pageManager.start();
--- popup.js
+++ popup.js
@@ -1,70 +1,45 @@
 const START_RECORDING_TEXT = 'Record New';
 const STOP_RECORDING_TEXT = 'Stop Recording';
 
-
-function firstRender() {
-	var button = document.getElementById('callToAction');
-	button.innerHTML = START_RECORDING_TEXT;
-	
-	button.addEventListener('click', actionPerformed);
+class PopupManager {
+	constructor(pageManager) {
+		this.pageManager = pageManager;
+		this.button = document.getElementById('callToAction'); 
+		this.scenarioDIV = document.getElementById('scenario');
+	}
+    
+	start() {
+		this.button.addEventListener('click', actionPerformed.bind(this));
+	}
+    
+	render() {
+		if (this.pageManager.isRecording) {
+			this.button.innerHTML = STOP_RECORDING_TEXT;
+			this.scenarioDIV.innerHTML = this.pageManager.scenario;
+		} else {
+			this.button.innerHTML = START_RECORDING_TEXT;
+			this.scenarioDIV.innerHTML = this.pageManager.scenario;
+		}
+	}
 }
 
 function actionPerformed() {
-	var button = document.getElementById('callToAction');
-	var scenarioDIV = document.getElementById('scenario');
-    
-	chrome.runtime.getBackgroundPage( bgPage => {
-		if (bgPage.isRecording) {
-			bgPage.isRecording = false;
-			button.innerHTML = START_RECORDING_TEXT;
-			scenarioDIV.innerHTML = bgPage.scenario;
-			stopRecording();
-		} else {
-			bgPage.isRecording = true;
-			button.innerHTML = STOP_RECORDING_TEXT;
-			scenarioDIV.innerHTML = bgPage.scenario;
-			startRecording();
-		}
-	});
-	
+	if (this.pageManager.isRecording) {
+		this.pageManager.stopRecording();
+	} else {
+		this.pageManager.startRecording();
+	}
+	this.render();
 }
 
-function startRecording() {
-	chrome.webNavigation.onCommitted.addListener(webNavigationCommitted);
-	chrome.webNavigation.onCompleted.addListener(webNavigationCompleted);
 
-	chrome.tabs.query({active: true, currentWindow: true}, activeTabs => {
-		if (activeTabs.length && activeTabs.length > 0) {
-			chrome.tabs.reload(activeTabs[0].id);
-		}
+
+function popupIsLoaded() {
+	chrome.runtime.getBackgroundPage(function (bg) {
+		var popupManager = new PopupManager(bg.pageManager);
+		popupManager.start();
+		popupManager.render();
 	});
 }
 
-function stopRecording() {
-	chrome.webNavigation.onCommitted.removeListener(webNavigationCommitted);
-	chrome.webNavigation.onCompleted.removeListener(webNavigationCompleted);
-	scenario = [];
-}
-
-function webNavigationCompleted() {
-	chrome.tabs.query({active: true}, activeTabs => {
-		if (activeTabs.length && activeTabs.length > 0) {
-			chrome.tabs.executeScript(activeTabs[0].id , {file:'attachListener.js'});
-		}
-	});
-}
-
-function webNavigationCommitted({transitionType, url}) {
-	chrome.tabs.query({active: true, currentWindow: true}, activeTabs => {
-		if (transitionType === 'reload' || transitionType === 'start_page') {
-			if (activeTabs.length && activeTabs.length > 0) {
-				addActionToScenario('Goto ' + url );
-			}
-		}
-	});  
-}
-
-
-
-
-document.addEventListener('DOMContentLoaded', firstRender);
+document.addEventListener('DOMContentLoaded', popupIsLoaded);
